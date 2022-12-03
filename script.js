3609160d97beabbf8bd96c41ac52a6c4184ca1b6
Eric Waithaka <74556293+eric-waithaka@users.noreply.github.com>
2022-12-03 16:45:20 +0300

    Added console.log to every round

--- script.js
+++ script.js
@@ -22,73 +22,63 @@ const computerPlay = () => {
         break; 
         case 3: return('scissors');
         break;
-        default: return 'Please enter either rock, paper, or scissors'
     }
 }
 
 const playRound = () => {
     playerSelection = prompt("Welcome to my Rock, Paper, Scissors Game. Please enter rock, paper, or scissors?").toLowerCase().trim();
 
-    if(playerSelection === 'rock' | 'paper' | 'scissors') {
+    if(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection ==='scissors') {
         computerSelection = computerPlay();  
 
         if(playerSelection === computerSelection) {
             alert("It's a DRAW!")
+            console.log("Round is a draw")
         }
         else if(playerSelection === 'paper' && computerSelection === 'rock') {
-            alert (
-                "You WIN! Paper beats Rock."           
-            )
-            playerScore(); 
-
+            alert ("You WIN! Paper beats Rock.")
+            playerScore();
+            console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
         }
         else if(playerSelection === 'paper' && computerSelection === 'scissors') {
-            alert (
-                "You LOSE! Scissors beats Paper"
-            )
+            alert ("You LOSE! Scissors beats Paper")
             computerScore();
+            console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
         }
         else if(playerSelection === 'rock' && computerSelection === 'paper') {
-            alert (
-                "You LOSE! Paper beats Rock"            
-            )
+            alert ("You LOSE! Paper beats Rock")
             computerScore();
+            console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
         }
         else if(playerSelection === 'rock' && computerSelection === 'scissors') {
-            alert (
-                "You WIN! Rock beats Scissors"
-            )
+            alert ("You WIN! Rock beats Scissors")
             playerScore();
+            console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
         }
         else if(playerSelection === 'scissors' && computerSelection === 'rock') {
-            alert (
-                "You LOSE! Rock beats Scissors"
-            )
+            alert ("You LOSE! Rock beats Scissors")
             computerScore();
+            console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
         }
         else if(playerSelection === 'scissors' && computerSelection === 'paper') {
-            alert (
-                "You WIN! Scissors beats Paper"
-            )
+            alert ("You WIN! Scissors beats Paper")
             playerScore();
+            console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
+        }
+        else {
+            alert ("Please enter either: rock, paper, or scissors");
         }
     }    
 }
 
 const game = () => {
     for (let i=0; i < 5; i++) {        
-        playRound(); 
+        playRound();
     }
     `{${pScore} > ${cScore}}` ? 
-        (alert(
-            `Player Score: ${pScore}; Computer Score: ${cScore}. PLAYER WINS!!!`
-        ))
+        (alert(`Player Score: ${pScore}; Computer Score: ${cScore}. PLAYER WINS!!!`))
         :
-        (alert(
-            `Player Score: ${pScore}; Computer Score: ${cScore}, COMPUTER WINS!!!`
-        ))
-    console.log(playerScore());
-    console.log(computerScore());
+        (alert(`Player Score: ${pScore}; Computer Score: ${cScore}, COMPUTER WINS!!!`))
 }
 
 game();

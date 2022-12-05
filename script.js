bb4af29cfd952d7d59a9a2ecf78e84d40b2fc4ea
Eric Waithaka <74556293+eric-waithaka@users.noreply.github.com>
2022-12-05 19:11:43 +0300

    Added input validation

--- script.js
+++ script.js
@@ -25,50 +25,54 @@ const computerPlay = () => {
     }
 }
 
+
+
 const playRound = () => {
     playerSelection = prompt("Welcome to my Rock, Paper, Scissors Game. Please enter rock, paper, or scissors?").toLowerCase().trim();
 
-    if(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection ==='scissors') {
-        computerSelection = computerPlay();  
-
-        if(playerSelection === computerSelection) {
-            alert("It's a DRAW!")
-            console.log("Round is a draw")
+    if (playerSelection == 'rock' || playerSelection === 'paper' || playerSelection ==='scissors') {
+        if(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection ==='scissors') {
+            computerSelection = computerPlay();  
+    
+            if(playerSelection === computerSelection) {
+                alert("It's a DRAW!")
+                console.log("Round is a draw")
+            }
+            else if(playerSelection === 'paper' && computerSelection === 'rock') {
+                alert ("You WIN! Paper beats Rock.")
+                playerScore();
+                console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
+            }
+            else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+                alert ("You LOSE! Scissors beats Paper")
+                computerScore();
+                console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
+            }
+            else if(playerSelection === 'rock' && computerSelection === 'paper') {
+                alert ("You LOSE! Paper beats Rock")
+                computerScore();
+                console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
+            }
+            else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+                alert ("You WIN! Rock beats Scissors")
+                playerScore();
+                console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
+            }
+            else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+                alert ("You LOSE! Rock beats Scissors")
+                computerScore();
+                console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
+            }
+            else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+                alert ("You WIN! Scissors beats Paper")
+                playerScore();
+                console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
+            }
         }
-        else if(playerSelection === 'paper' && computerSelection === 'rock') {
-            alert ("You WIN! Paper beats Rock.")
-            playerScore();
-            console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
-        }
-        else if(playerSelection === 'paper' && computerSelection === 'scissors') {
-            alert ("You LOSE! Scissors beats Paper")
-            computerScore();
-            console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
-        }
-        else if(playerSelection === 'rock' && computerSelection === 'paper') {
-            alert ("You LOSE! Paper beats Rock")
-            computerScore();
-            console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
-        }
-        else if(playerSelection === 'rock' && computerSelection === 'scissors') {
-            alert ("You WIN! Rock beats Scissors")
-            playerScore();
-            console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
-        }
-        else if(playerSelection === 'scissors' && computerSelection === 'rock') {
-            alert ("You LOSE! Rock beats Scissors")
-            computerScore();
-            console.log(`Computer wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
-        }
-        else if(playerSelection === 'scissors' && computerSelection === 'paper') {
-            alert ("You WIN! Scissors beats Paper")
-            playerScore();
-            console.log(`Player wins round - Player Choice:${playerSelection}, Computer selection:${computerSelection}`)
-        }
-        else {
-            alert ("Please enter either: rock, paper, or scissors");
-        }
-    }    
+    }
+    else {
+        alert("Please enter either rock, paper, or scissors");
+    }   
 }
 
 const game = () => {
